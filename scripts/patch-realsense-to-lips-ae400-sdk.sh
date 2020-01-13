447a35cd36079058fc25831a3d8c4b47bd9e611d
Tim Cheng <timcheng@lips-hci.com>
2020-01-13 16:40:47 +0800

    [scripts] lips-ae400: support commands rm and cp
    Support commands rm and cp in Git BASH environment on Windows.
    
    Redmine id:

--- scripts/patch-realsense-to-lips-ae400-sdk.sh
+++ scripts/patch-realsense-to-lips-ae400-sdk.sh
@@ -23,7 +23,9 @@ RS2_GIT=$1
 echo "AE400 SRC GIT = $AE4_GIT"
 echo "  RS2 SRC GIT = $RS2_GIT"
 echo ""
-echo "Processing ..."
+
+if [ "$(which rsync)" != "" ]; then
+echo "Processing by rsync ..."
 
 rsync -avrhP --delete --exclude 'backend-*.h' $AE4_GIT/src/linux/ $RS2_GIT/src/linux/
 #rsync -avrhP --delete $AE4_GIT/src/linux/link_lips_prebuilt.cmake $RS2_GIT/src/linux/
@@ -41,6 +43,48 @@ rsync -avrhP --delete $AE4_GIT/CMake/install_network_config.cmake $RS2_GIT/CMake
 printf "\ninclude(CMake/install_network_config.cmake)\n" >> $RS2_GIT/CMake/install_config.cmake
 
 rsync -avrhP --delete $AE4_GIT/wrappers/python/link_lips_prebuilt.cmake $RS2_GIT/wrappers/python/
+printf "\ninclude(link_lips_prebuilt.cmake)\n" >> $RS2_GIT/wrappers/python/CMakeLists.txt
+
+echo "Done!"
+exit 0
+
+fi
+
+#
+# Cannot find rsync, use common commands like cp & move
+#
+echo "Processing by rm&cp ..."
+
+for dstfile in $RS2_GIT/src/linux/*; do
+    [ "$(basename "$dstfile" | grep -i 'backend-.*\.h')" == "" ] && rm -vf $dstfile
+done
+for srcfile in $AE4_GIT/src/linux/*; do
+    cp -vf $srcfile $RS2_GIT/src/linux/
+done
+#rsync -avrhP --delete --exclude 'backend-*.h' $AE4_GIT/src/linux/ $RS2_GIT/src/linux/
+
+for dstfile in $RS2_GIT/src/win/*; do
+    [ "$(basename "$dstfile" | grep -i 'win-helpers..*')" == "" ] && rm -vf $dstfile
+done
+for srcfile in $AE4_GIT/src/win/*; do
+    cp -vf $srcfile $RS2_GIT/src/win/
+done
+#rsync -avrhP --delete --exclude 'win-helpers.*' $AE4_GIT/src/win/ $RS2_GIT/src/win/
+
+for srcfile in $AE4_GIT/third-party/lips/*; do
+    cp -vrfP $srcfile $RS2_GIT/third-party/lips/
+done
+#rsync -avrhP --delete $AE4_GIT/third-party/lips/ $RS2_GIT/third-party/lips/
+
+cp -vf $AE4_GIT/config/network.json $RS2_GIT/config/
+#rsync -avrhP --delete $AE4_GIT/config/network.json $RS2_GIT/config/
+
+cp -vf $AE4_GIT/CMake/install_network_config.cmake $RS2_GIT/CMake/
+#rsync -avrhP --delete $AE4_GIT/CMake/install_network_config.cmake $RS2_GIT/CMake/
+printf "\ninclude(CMake/install_network_config.cmake)\n" >> $RS2_GIT/CMake/install_config.cmake
+
+cp -vf $AE4_GIT/wrappers/python/link_lips_prebuilt.cmake $RS2_GIT/wrappers/python/
+#rsync -avrhP --delete $AE4_GIT/wrappers/python/link_lips_prebuilt.cmake $RS2_GIT/wrappers/python/
 printf "\ninclude(link_lips_prebuilt.cmake)\n" >> $RS2_GIT/wrappers/python/CMakeLists.txt
 
 echo "Done!"
